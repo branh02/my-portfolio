51cc5c8255162cfc7fa051e7cfc5a6cb81f544d5
Branavan H <136491525+branh02@users.noreply.github.com>
2024-08-07 03:23:16 +0100

    Update animatedBackground.js

--- src/components/animatedBackground.js
+++ src/components/animatedBackground.js
@@ -6,8 +6,8 @@ const AnimatedBackground = () => {
     <div className="ripple-background">
       <div className="ripple-1"></div>
       <div className="ripple-2"></div>
-      <div className="ripple-3"></div>
-      <div className="ripple-4"></div>
+      // <div className="ripple-3"></div>
+      // <div className="ripple-4"></div>
       <div className="ripple-5"></div>
     </div>
   );
